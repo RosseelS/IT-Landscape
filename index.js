9656d15ca52013144fb0e9e7e32fc6ee4ccc7998
RosseelS <rosseel.senne2005@gmail.com>
2025-05-31 20:24:35 +0200

    test nr 2

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ app.get("/will", function (req, res) {
 app.get("/ready", function (req, res) {
   res.send('{ "response": " Great!, It works!" }');
 });
-app.get("test", function (req, res) {
+app.get("/test", function (req, res) {
   res.send('{"response": "Great!, The test works!" }');
 });
+app.get("/test2", function (req, res) {
+  res.send("{ 'Response:' 'Great test 2 is working' }")
+})
 app.listen(process.env.PORT || 3000);
 module.exports = app;
